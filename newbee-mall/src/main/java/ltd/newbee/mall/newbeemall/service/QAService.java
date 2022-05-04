045b13d1c327ada66de7a980bd8237851d937e09
yidan98 <sonngeitann@gmail.com>
2022-05-04 17:11:45 +0900

    插入数据点赞人数QA追加

--- newbee-mall/src/main/java/ltd/newbee/mall/newbeemall/service/QAService.java
+++ newbee-mall/src/main/java/ltd/newbee/mall/newbeemall/service/QAService.java
@@ -1,10 +1,15 @@
 package ltd.newbee.mall.newbeemall.service;
 
 import java.util.List;
+import java.util.Map;
 
 import ltd.newbee.mall.newbeemall.vo.QAVOFirst;
 
 public interface QAService {
 	List<QAVOFirst> selectQA(long goodsId, int pageNo, int number, String columnName);
+
+	int getLikeQA(int answerId, int userId);
+
+	int insertQALike(Map<String, Object> QALike);
 
 }
